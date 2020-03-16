9702642270edb13f4af29f90aea39c600b2d0af2
Kate Ratynskaya <catrat16999@gmail.com>
2020-03-16 17:16:11 +0300

    add first version

---
+++ symbol/symbol/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace symbol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stroka;
+            bool Proverka = false;
+            int Simvol = 0;
+            Console.Write("Введите строку: ");
+            stroka = Console.ReadLine();
+            Console.WriteLine(stroka);
+            for (int i = 0; i < stroka.Length; i++)
+            {
+                for (int j = 0; j < stroka.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        if (stroka[i] == stroka[j])
+                        {
+                            Proverka = true;
+                        }
+                        if ((j == stroka.Length - 1) && (Proverka == false))
+                        {
+                            Simvol++;
+                        }
+                    }
+                }
+                Proverka = false;
+            }
+            Console.WriteLine("Неповторяющихся символов " + Convert.ToString(Simvol));
+
+        }
+    }
+}
